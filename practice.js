f2402d7c3d72c86fe455b18d5b57e3d6aafa2f76
Shehin Thoombil <shehinthoombil@gmail.com>
2024-03-25 12:17:58 +0530

    to find the smallest index value of tyhe target

--- practice.js
+++ practice.js
@@ -116,3 +116,28 @@
 //     }
 // }
 // console.log(result)
+
+
+// to find the smallest index value of tyhe target
+// let arr = [1, 1, 1, 2, 4, 5]
+// let target = 1
+
+// function binarySearch(arr, target) {
+//     let leftIndex = 0;
+//     let rightIndex = arr.length - 1
+//     let result = -1
+//     while (leftIndex <= rightIndex) {
+//         let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+//         if (target == arr[middleIndex] ) {
+//             result = middleIndex
+//             rightIndex = middleIndex -1 
+            
+//         } else if (target < arr[middleIndex]) {
+//             rightIndex = middleIndex - 1
+//         } else {
+//             leftIndex = middleIndex + 1
+//         }
+//     }
+//     return result
+// }
+// console.log(binarySearch(arr, target))
