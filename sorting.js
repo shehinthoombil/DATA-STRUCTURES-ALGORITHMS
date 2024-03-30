c4c5575e7ee2333989aae2766941fb94d65cc718
Shehin Thoombil <shehinthoombil@gmail.com>
2024-03-30 13:27:03 +0530

    quick sort

--- sorting.js
+++ sorting.js
@@ -54,7 +54,27 @@
 // }
 // console.log(selectionSort(arr))
 
+// Quick sort
 
+let arr = [8, 20, -2, 4, -6]
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let leftArray = []
+    let rightArray = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            leftArray.push(arr[i])
+        } else {
+            rightArray.push(arr[i])
+        }
+        
+    }
+    return [...quickSort(leftArray), pivot, ...quickSort(rightArray)]
+}
+console.log(quickSort(arr))
 
 
 
