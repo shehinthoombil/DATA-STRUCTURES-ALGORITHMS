d9b7855f1fb69a562b4da7bf10be372063e0885e
Shehin Thoombil <shehinthoombil@gmail.com>
2024-04-01 10:08:05 +0530

    mergeSort

--- algorithms.js
+++ algorithms.js
@@ -142,26 +142,26 @@
 
 //recursive Binary search 
 
-// function recursiveBinarySearch(arr,target) {
-//     return search(arr, target, 0, arr.length-1) 
-//     }
-//     function search(arr,target,leftIndex,rightIndex){
-//     if(leftIndex> rightIndex) {
-//         return -1
-//     }
+function recursiveBinarySearch(arr,target) {
+    return search(arr, target, 0, arr.length-1) 
+    }
+    function search(arr,target,leftIndex,rightIndex){
+    if(leftIndex> rightIndex) {
+        return -1
+    }
 
-//     let middleIndex = Math.floor((leftIndex+rightIndex) /2)
-//     if(target == arr[middleIndex]){
-//         return middleIndex
-//     }
-//     if(target< arr[middleIndex]){
-//         return search(arr,target,leftIndex,middleIndex - 1)
-//     }else{
-//         return search(arr,target,middleIndex+1,rightIndex)
-//     }
-// }
-// console.log(recursiveBinarySearch([-1,4,7,8,10],8))
-// console.log(recursiveBinarySearch([-1,4,7,8,10],10))
-// console.log(recursiveBinarySearch([-1,4,7,8,10],9))
-//O(logn)
+    let middleIndex = Math.floor((leftIndex+rightIndex) /2)
+    if(target == arr[middleIndex]){
+        return middleIndex
+    }
+    if(target< arr[middleIndex]){
+        return search(arr,target,leftIndex,middleIndex - 1)
+    }else{
+        return search(arr,target,middleIndex+1,rightIndex)
+    }
+}
+console.log(recursiveBinarySearch([-1,4,7,8,10],8))
+console.log(recursiveBinarySearch([-1,4,7,8,10],10))
+console.log(recursiveBinarySearch([-1,4,7,8,10],9))
+// complexity - O(logn)
 
--- sorting.js
+++ sorting.js
@@ -56,26 +56,51 @@
 
 // Quick sort
 
-let arr = [8, 20, -2, 4, -6]
-function quickSort(arr) {
+// let arr = [8, 20, -2, 4, -6]
+// function quickSort(arr) {
+//     if (arr.length < 2) {
+//         return arr
+//     }
+//     let pivot = arr[arr.length - 1]
+//     let leftArray = []
+//     let rightArray = []
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         if (arr[i] < pivot) {
+//             leftArray.push(arr[i])
+//         } else {
+//             rightArray.push(arr[i])
+//         }
+//     }
+//     return [...quickSort(leftArray), pivot, ...quickSort(rightArray)]
+// }
+// console.log(quickSort(arr))
+
+//  Merge Sort
+
+function mergeSort(arr) {
     if (arr.length < 2) {
         return arr
     }
-    let pivot = arr[arr.length - 1]
-    let leftArray = []
-    let rightArray = []
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] < pivot) {
-            leftArray.push(arr[i])
-        } else {
-            rightArray.push(arr[i])
-        }
-        
-    }
-    return [...quickSort(leftArray), pivot, ...quickSort(rightArray)]
+    const mid = Math.floor(arr.length / 2)
+    const leftArray = arr.slice(0, mid)
+    const rightArray = arr.slice(mid)
+    return merge(mergeSort(leftArray), mergeSort(rightArray))
 }
-console.log(quickSort(arr))
 
+function merge(leftArray, rightArray) {
+    const sortArray = []
+    while (leftArray.length && rightArray.length) {
+        if (leftArray[0] <= rightArray[0]) {
+            sortArray.push(leftArray.shift())
+        } else {
+            sortArray.push(rightArray.shift())
+        }
+    }
+    return [...sortArray,...leftArray,...rightArray]
+}
+
+let arr = [8, 20, -2, 4, -6]
+console.log(mergeSort(arr))
 
 
 
